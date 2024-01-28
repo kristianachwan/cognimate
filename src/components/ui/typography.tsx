c64e21b2cfcce07c9036f97b5c08982104a36800
Kristian Hadinata Achwan <kristianachwan@gmail.com>
2024-01-29 03:51:43 +0800

    feat: add typography component

---
+++ src/components/ui/typography.tsx
@@ -0,0 +1,63 @@
+import { cn } from "~/lib/utils";
+import React from "react";
+
+interface TypographyProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export function TypographyH1({ children, className }: TypographyProps) {
+  return (
+    <h1
+      className={cn(
+        "scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl",
+        className,
+      )}
+    >
+      {children}
+    </h1>
+  );
+}
+
+export function TypographyH2({ children, className }: TypographyProps) {
+  return (
+    <h2
+      className={cn(
+        "scroll-m-20 text-3xl font-semibold tracking-tight",
+        className,
+      )}
+    >
+      {children}
+    </h2>
+  );
+}
+
+export function TypographyH3({ children, className }: TypographyProps) {
+  return (
+    <h3
+      className={cn(
+        "scroll-m-20 text-2xl font-semibold tracking-tight",
+        className,
+      )}
+    >
+      {children}
+    </h3>
+  );
+}
+
+export function TypographyH4({ children, className }: TypographyProps) {
+  return (
+    <h4
+      className={cn(
+        "scroll-m-20 text-xl font-semibold tracking-tight",
+        className,
+      )}
+    >
+      {children}
+    </h4>
+  );
+}
+
+export function TypographyP({ children, className }: TypographyProps) {
+  return <p className={cn("scroll-m-20", className)}>{children}</p>;
+}
