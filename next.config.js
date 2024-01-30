449b25bf888ebdddf64261b50a503e67d72061f0
Kristian Hadinata Achwan <kristianachwan@gmail.com>
2024-01-30 22:18:10 +0800

    chore: setup jotai

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  experimental: {
+    swcPlugins: [["@swc-jotai/react-refresh", {}]],
+  },
+};
 
 export default config;
