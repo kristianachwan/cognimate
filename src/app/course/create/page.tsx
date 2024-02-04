dd0afce55135f14524a828e6dadf63cd2aec1c05
Kristian Hadinata Achwan <kristianachwan@gmail.com>
2024-02-04 23:36:07 +0800

    feat: prettify ui of create course

--- src/app/course/create/page.tsx
+++ src/app/course/create/page.tsx
@@ -64,104 +64,109 @@ export default function CreatePage() {
 
   form.watch();
   return (
-    <div>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="mt-4 w-full">
-          <FormField
-            control={form.control}
-            name="title"
-            render={({ field }) => {
-              return (
-                <FormItem className="flex w-full flex-col items-start sm:flex-row sm:items-center">
-                  <FormLabel className="flex-[1] text-xl">Title</FormLabel>
-                  <FormControl className="flex-[6]">
-                    <Input
-                      placeholder="Enter the main topic of the course"
-                      {...field}
-                    />
-                  </FormControl>
-                </FormItem>
-              );
-            }}
-          />
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="w-[40vw] rounded-lg border px-8 py-8">
+        <Form {...form}>
+          <h1 className="text-2xl font-bold">
+            Choose any topic that you want to learn...
+          </h1>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="mt-4 w-full">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => {
+                return (
+                  <FormItem className="flex w-full flex-col items-start sm:flex-row sm:items-center">
+                    <FormLabel className="flex-[1] text-xl">Title</FormLabel>
+                    <FormControl className="flex-[6]">
+                      <Input
+                        placeholder="Enter the main topic of the course"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                );
+              }}
+            />
 
-          <AnimatePresence>
-            {form.watch("units").map((_, index) => {
-              return (
-                <motion.div
-                  key={index}
-                  initial={{ opacity: 0, height: 0 }}
-                  animate={{ opacity: 1, height: "auto" }}
-                  exit={{ opacity: 0, height: 0 }}
-                  transition={{
-                    opacity: { duration: 0.2 },
-                    height: { duration: 0.2 },
+            <AnimatePresence>
+              {form.watch("units").map((_, index) => {
+                return (
+                  <motion.div
+                    key={index}
+                    initial={{ opacity: 0, height: 0 }}
+                    animate={{ opacity: 1, height: "auto" }}
+                    exit={{ opacity: 0, height: 0 }}
+                    transition={{
+                      opacity: { duration: 0.2 },
+                      height: { duration: 0.2 },
+                    }}
+                  >
+                    <FormField
+                      key={index}
+                      control={form.control}
+                      name={`units.${index}`}
+                      render={({ field }) => {
+                        return (
+                          <FormItem className="flex w-full flex-col items-start sm:flex-row sm:items-center">
+                            <FormLabel className="flex-[1] text-xl">
+                              Unit {index + 1}
+                            </FormLabel>
+                            <FormControl className="flex-[6]">
+                              <Input
+                                placeholder="Enter subtopic of the course"
+                                {...field}
+                              />
+                            </FormControl>
+                          </FormItem>
+                        );
+                      }}
+                    />
+                  </motion.div>
+                );
+              })}
+            </AnimatePresence>
+
+            <div className="mt-4 flex items-center justify-center">
+              <Separator className="flex-[1]" />
+              <div className="mx-4">
+                <Button
+                  type="button"
+                  variant="secondary"
+                  className="font-semibold"
+                  onClick={() => {
+                    form.setValue("units", [...form.watch("units"), ""]);
                   }}
                 >
-                  <FormField
-                    key={index}
-                    control={form.control}
-                    name={`units.${index}`}
-                    render={({ field }) => {
-                      return (
-                        <FormItem className="flex w-full flex-col items-start sm:flex-row sm:items-center">
-                          <FormLabel className="flex-[1] text-xl">
-                            Unit {index + 1}
-                          </FormLabel>
-                          <FormControl className="flex-[6]">
-                            <Input
-                              placeholder="Enter subtopic of the course"
-                              {...field}
-                            />
-                          </FormControl>
-                        </FormItem>
-                      );
-                    }}
-                  />
-                </motion.div>
-              );
-            })}
-          </AnimatePresence>
+                  Add Unit
+                  <Plus className="ml-2 h-4 w-4 text-green-500" />
+                </Button>
 
-          <div className="mt-4 flex items-center justify-center">
-            <Separator className="flex-[1]" />
-            <div className="mx-4">
-              <Button
-                type="button"
-                variant="secondary"
-                className="font-semibold"
-                onClick={() => {
-                  form.setValue("units", [...form.watch("units"), ""]);
-                }}
-              >
-                Add Unit
-                <Plus className="ml-2 h-4 w-4 text-green-500" />
-              </Button>
-
-              <Button
-                type="button"
-                variant="secondary"
-                className="ml-2 font-semibold"
-                onClick={() => {
-                  form.setValue("units", form.watch("units").slice(0, -1));
-                }}
-              >
-                Remove Unit
-                <Trash className="ml-2 h-4 w-4 text-red-500" />
-              </Button>
+                <Button
+                  type="button"
+                  variant="secondary"
+                  className="ml-2 font-semibold"
+                  onClick={() => {
+                    form.setValue("units", form.watch("units").slice(0, -1));
+                  }}
+                >
+                  Remove Unit
+                  <Trash className="ml-2 h-4 w-4 text-red-500" />
+                </Button>
+              </div>
+              <Separator className="flex-[1]" />
             </div>
-            <Separator className="flex-[1]" />
-          </div>
-          <Button
-            disabled={isLoading}
-            type="submit"
-            className="mt-6 w-full"
-            size="lg"
-          >
-            Lets Go!
-          </Button>
-        </form>
-      </Form>
+            <Button
+              disabled={isLoading}
+              type="submit"
+              className="mt-6 w-full"
+              size="lg"
+            >
+              Lets Go!
+            </Button>
+          </form>
+        </Form>
+      </div>
     </div>
   );
 }
--- src/app/course/page.tsx
+++ src/app/course/page.tsx
@@ -1,12 +1,29 @@
 "use client";
+import { Plus } from "lucide-react";
+import Link from "next/link";
 import { CourseCard } from "~/components/CourseCard";
+import { Button } from "~/components/ui/button";
 import { api } from "~/trpc/react";
 
 export default function CoursePage() {
   const { data: courses } = api.course.getMany.useQuery();
   return (
-    <div className="flex h-full w-screen flex-wrap justify-center gap-4">
-      {courses?.map((course) => <CourseCard key={course.id} course={course} />)}
+    <div className="px-6">
+      <Link href="/course/create">
+        <Button className="gap-4">
+          <div>I want to learn...</div>
+          <Plus />
+        </Button>
+      </Link>
+      <div className="flex h-full min-h-[90vh] w-screen flex-wrap items-center justify-center gap-4">
+        {courses?.length == 0 ? (
+          <div>You dont have any course for now...</div>
+        ) : (
+          courses?.map((course) => (
+            <CourseCard key={course.id} course={course} />
+          ))
+        )}
+      </div>
     </div>
   );
 }
