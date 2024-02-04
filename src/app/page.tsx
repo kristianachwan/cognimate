e64159053d52fb43492211e75b9f83f54e619f3a
Kristian Hadinata Achwan <kristianachwan@gmail.com>
2024-02-04 22:43:10 +0800

    feat: better hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,12 @@ import { Button } from "~/components/ui/button";
 import Link from "next/link";
 export default async function Home() {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center gap-10">
-      <h1 className="text-5xl">Your All-in-one-Study AI Assistant ✨</h1>
+    <div className="flex h-[90vh] w-screen flex-col items-center justify-center gap-10">
+      <h1 className="text-6xl font-bold">Cognimate 🤖.</h1>
+      <p className="text-3xl">
+        Your all-in-one AI Study Assistant designed to elevate your learning
+        experience ✨
+      </p>
       <Link href="signup">
         <Button>Get Started</Button>
       </Link>
