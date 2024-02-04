29881a6d12d5d892ec836e5f5de088cda6842ba3
Kristian Hadinata Achwan <kristianachwan@gmail.com>
2024-02-04 20:33:26 +0800

    feat: implement sign up page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import { Button } from "~/components/ui/button";
+import Link from "next/link";
 export default async function Home() {
-  return <>Home</>;
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-10">
+      <h1 className="text-5xl">Your All-in-one-Study AI Assistant ✨</h1>
+      <Link href="signup">
+        <Button>Get Started</Button>
+      </Link>
+    </div>
+  );
 }
