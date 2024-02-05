09c281811aaecfbc9eff40753b17f7c970e0bdea
Kristian Hadinata Achwan <kristianachwan@gmail.com>
2024-02-06 03:46:19 +0800

    feat: use vanilla atom

--- src/app/state/user.ts
+++ src/app/state/user.ts
@@ -1,18 +1,26 @@
-import { atomWithStorage } from "jotai/utils";
+// import { atomWithStorage} from "jotai/utils";
+import { atom } from "jotai/vanilla";
 import { NAMESPACE } from "../constant";
 
-export const usernameAtom = atomWithStorage(NAMESPACE + "username", "");
-export const emailAtom = atomWithStorage(NAMESPACE + "email", "");
-export const educationalLevelAtom = atomWithStorage(
-  NAMESPACE + "educationalLevel",
-  "",
-);
-export const yearOfStudyAtom = atomWithStorage(NAMESPACE + "yearOfStudy", "");
-export const specialConditionAtom = atomWithStorage(
-  NAMESPACE + "specialCondition",
-  "",
-);
-export const registeredAtom = atomWithStorage<boolean>(
-  NAMESPACE + "registered",
-  false,
-);
+// export const usernameAtom = atomWithStorage(NAMESPACE + "username", "");
+// export const emailAtom = atomWithStorage(NAMESPACE + "email", "");
+// export const educationalLevelAtom = atomWithStorage(
+//   NAMESPACE + "educationalLevel",
+//   "",
+// );
+// export const yearOfStudyAtom = atomWithStorage(NAMESPACE + "yearOfStudy", "");
+// export const specialConditionAtom = atomWithStorage(
+//   NAMESPACE + "specialCondition",
+//   "",
+// );
+// export const registeredAtom = atomWithStorage<boolean>(
+//   NAMESPACE + "registered",
+//   false,
+// );
+
+export const usernameAtom = atom("");
+export const emailAtom = atom("");
+export const educationalLevelAtom = atom("");
+export const yearOfStudyAtom = atom("");
+export const specialConditionAtom = atom("");
+export const registeredAtom = atom(false);
